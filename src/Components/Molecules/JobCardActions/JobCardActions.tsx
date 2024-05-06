13e49bef5d0a29dc080a62846e88e05c867bd55e
IshanAgarwal1002 <ishanagarwal1002@gmail.com>
2024-05-06 15:22:17 +0530

    Created JobCardActions

---
+++ src/Components/Molecules/JobCardActions/JobCardActions.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from "react";
+import { CardActions } from '@mui/material';
+import { CustomButton } from "../../Atoms";
+import { CONSTANTS } from "../../../Shared/Constants";
+import './JobCardActions.scss';
+
+interface Props {
+  applyLink: string;
+  referralLink: string;
+}
+
+const JobCardActions: FC<Props> = ({
+  applyLink,
+  referralLink,
+}) => {
+  return (
+    <CardActions className="card-actions-wrapper" disableSpacing>
+      <CustomButton customCls="primary apply-button" href={applyLink}>
+        {CONSTANTS.EASY_APPLY}
+      </CustomButton>
+      <CustomButton customCls="secondary referral-button" href={referralLink}>
+        {CONSTANTS.UNLOCK_REFERRAL_ASKS}
+      </CustomButton>
+    </CardActions>
+  );
+};
+
+export default JobCardActions;
