e93561659ee8a645cbb21f9ec664f7c27a47f7ed
IshanAgarwal1002 <ishanagarwal1002@gmail.com>
2024-05-06 15:48:44 +0530

    Created JobCardContent molecule

---
+++ src/Components/Molecules/JobCardContent/JobCardContent.tsx
@@ -0,0 +1,50 @@
+import React, { FC } from "react";
+import { Box } from '@mui/material';
+import { CustomTypography, CustomLink } from "../../Atoms";
+import { CONSTANTS } from "../../../Shared/Constants";
+import './JobCardContent.scss';
+
+interface Props {
+  jdLink: string;
+  jobDetailsFromCompany: string;
+  minExp: number | null;
+  maxExp: number | null;
+}
+
+const JobCardContent: FC<Props> = ({
+  jdLink,
+  jobDetailsFromCompany,
+  minExp,
+  maxExp,
+}) => {
+  return (
+    <Box className="card-content" component='div'>
+      <Box className="about-job-wrapper" component='div'>
+        <CustomTypography customCls="heading about-company">
+          {CONSTANTS.ABOUT_COMPANY}
+        </CustomTypography>
+        <CustomTypography customCls="type-normal about-us">
+          {CONSTANTS.ABOUT_US}
+        </CustomTypography>
+        <Box className="about-job" component='div'>
+          {jobDetailsFromCompany}
+        </Box>
+        <Box className="view-job-wrapper" component='div'>
+          <CustomLink customCls="link-blue job-link" href={jdLink}>
+            {CONSTANTS.VIEW_JOB}
+          </CustomLink>
+        </Box>
+      </Box>
+      <Box className="job-experience-wrapper" component='div'>
+        <CustomTypography customCls="title">
+          {CONSTANTS.MINIMUM_EXPERIENCE}
+        </CustomTypography>
+        <CustomTypography customCls="sub-title">
+          {minExp ? `${minExp} ${CONSTANTS.YEARS}` : CONSTANTS.NOT_SPECIFIED}
+        </CustomTypography>
+      </Box>
+    </Box>
+  );
+};
+
+export default JobCardContent;
