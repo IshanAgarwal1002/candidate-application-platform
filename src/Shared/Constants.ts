8956d38d640b1412b20ff3762dc20f3245298eeb
IshanAgarwal1002 <ishanagarwal1002@gmail.com>
2024-05-06 14:26:15 +0530

    Created text constants

---
+++ src/Shared/Constants.ts
@@ -0,0 +1,19 @@
+export const CONSTANTS = {
+  POSTED: '⏳ Posted',
+  DAYS_AGO: 'days ago',
+  ESTIMATED_SALARY: 'Estimated Salary',
+  ABOUT_COMPANY: 'About Company:',
+  ABOUT_US: 'About us',
+  VIEW_JOB: 'View job',
+  SKILLS: 'Skills',
+  MINIMUM_EXPERIENCE: 'Minimum Experience',
+  YEARS: 'years',
+  EASY_APPLY: '⚡ Easy Apply',
+  UNLOCK_REFERRAL_ASKS: 'Unlock referral asks',
+  NOT_SPECIFIED: 'Not specified',
+};
+
+export const CURRENCY_MAP: {[key: string]: string} = {
+  'USD': '$',
+  'INR': '₹',
+};
